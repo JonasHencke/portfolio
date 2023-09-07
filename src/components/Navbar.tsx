ffc8b772e500248886ba4a377fa99f9dc885ad1c
jonas-1997 <jvhencke@googlemail.com>
2023-09-07 18:37:05 +0200

    Implement initial animation state to the Navbar component with framer motion

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,15 @@
+import { motion } from "framer-motion"
+
 export default function Navbar() {
     return(
-            <nav className="navbar">
+            <motion.nav className="navbar"
+            initial={{ y: -200, opacity: 0}}
+            animate = {{y: 0, opacity: 1}}
+            transition={{ ease: "easeOut", duration: 0.5 }}>
                 <div>Über mich</div>
                 <div>Projekte</div>
                 <div>Skills</div>
                 <div>Kontakt</div>
-            </nav>
+            </motion.nav>
     )
 }
