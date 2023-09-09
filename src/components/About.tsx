212bee83ba5be7f57ef1d72ce15f32456b651eb6
jonas-1997 <jvhencke@googlemail.com>
2023-09-09 22:48:48 +0200

    Replace placeholder text in the About component

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,29 +1,28 @@
-import { motion} from "framer-motion";
+import { motion } from "framer-motion";
 
 export default function About() {
-
   return (
-    <motion.div className="about-container"
-    initial={{scale: 0.75, opacity: 0.5}}
-    whileInView={{scale: 1, opacity: 1}}
-    viewport={{ once: true, amount: 0.5}}
-    transition={{ ease: "easeOut", duration: 0.7 }}>
-      <div className="about-header"
-       >Über mich</div>
-      <div className="about-text"
-      >
-        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
-        eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam
-        voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet
-        clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit
-        amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
-        nonumy. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
-        diam nonumy.<br/> no sea takimata sanctus est Lorem ipsum dolor sit amet.
-        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
-        eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam
-        voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet
-        clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit
-        amet.
+    <motion.div
+      className="about-container"
+      initial={{ scale: 0.75, opacity: 0.5 }}
+      whileInView={{ scale: 1, opacity: 1 }}
+      viewport={{ once: true, amount: 0.5 }}
+      transition={{ ease: "easeOut", duration: 0.7 }}
+    >
+      <div className="about-header">Über mich</div>
+      <div className="about-text">
+        Angefangen mit HTML, CSS und Javascript machte ich meine ersten
+        Erfahrungen in der Webentwicklung und der Wunsch Programmieren auf
+        professioneller Ebene auszuführen, entwickelte sich zügig. Einige Monate
+        sind seitdem vergangen und mittlerweile programmiere ich
+        leidenschafltich mit dem Javascript-Framework React.<br/><br/>Für meine
+        Web-Anwendungen verwende ich Typescript oder Javascript, Context API zum
+        managen von State und React-Router zum erstellen komplexer
+        Web-Anwendungen. Für das Styling nutze ich gerne den CSS Pre-Processor
+        SASS und Animationen auf meinen Seiten erstelle ich über die Framer
+        Motion Library.<br/><br/>Für viele weiteren Technologien bin ich offen. Neben
+        dem Frontend habe ich auch großes Interesse
+        für Technologien im Backend.
       </div>
     </motion.div>
   );
