0bb2c29e3f3784f2b7eb58b1da24664f1731519d
jonas-1997 <jvhencke@googlemail.com>
2023-09-13 19:01:54 +0200

    Install EmailJS and add form to Contact component

---
+++ src/components/Contact.tsx
@@ -0,0 +1,10 @@
+import { ContactUs } from "./ContactForm"
+
+export default function Contact() {
+    return (
+        <div className="contact-container">
+           <div className="about-header">Kontakt</div>
+           <ContactUs/> 
+        </div>
+    )
+}
