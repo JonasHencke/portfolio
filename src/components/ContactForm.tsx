f5ea90770cf73e7d2fe9ee1f1eef627fa118605c
jonas-1997 <jvhencke@googlemail.com>
2023-09-14 14:22:55 +0200

    Add styling to ContactForm component

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -1,5 +1,5 @@
-import { useRef } from 'react';
-import emailjs from '@emailjs/browser';
+import { useRef } from "react";
+import emailjs from "@emailjs/browser";
 
 export const ContactUs = () => {
   const form = useRef<any>(null);
@@ -7,23 +7,29 @@ export const ContactUs = () => {
   const sendEmail = (e: any) => {
     e.preventDefault();
 
-    emailjs.sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', form.current, 'YOUR_PUBLIC_KEY')
-      .then((result) => {
+    emailjs
+      .sendForm(
+        "YOUR_SERVICE_ID",
+        "YOUR_TEMPLATE_ID",
+        form.current,
+        "YOUR_PUBLIC_KEY"
+      )
+      .then(
+        (result) => {
           console.log(result.text);
-      }, (error) => {
+        },
+        (error) => {
           console.log(error.text);
-      });
+        }
+      );
   };
 
   return (
-    <form ref={form} onSubmit={sendEmail}>
-      <label>Name</label>
-      <input type="text" name="user_name" />
-      <label>Email</label>
-      <input type="email" name="user_email" />
-      <label>Message</label>
-      <textarea name="message" />
-      <input type="submit" value="Send" />
+    <form ref={form} onSubmit={sendEmail} className="contact-form">
+      <input type="text" name="user_name" className="contact-form-name"/>
+      <input type="email" name="user_email" className="contact-form-mail"/>
+      <textarea name="message" className="contact-form-message"/>
+      <input type="submit" value="Send" className="contact-form-button"/>
     </form>
   );
 };
