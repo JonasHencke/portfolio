e3461a96485ddf59133da433e6ec196def5609fe
jonas-1997 <jvhencke@googlemail.com>
2023-09-30 12:46:36 +0200

    Add closing animation to the notifications

--- src/components/Notifications.tsx
+++ src/components/Notifications.tsx
@@ -1,4 +1,4 @@
-import { motion } from "framer-motion";
+import { motion, AnimatePresence } from "framer-motion";
 
 interface NotificationsProps {
   notifications: { id: number; message: string }[];
@@ -16,18 +16,23 @@ export default function Notifications(props: NotificationsProps) {
   return (
     <>
       <div className="notifications">
+      <AnimatePresence initial={true}>
         {props.notifications.map((notification) => (
+          
           <motion.div
             className="notification"
             key={notification.id}
             initial={{ opacity: 0, scale: 0 }}
             animate={{ opacity: 1, scale: 1 }}
-            transition={{ type: "spring", duration: 1 }}
+            transition={{ type: "spring", duration: 0.8 }}
+            exit={{ opacity: 0, scale: 0.5, transition: { duration: 0.5 } }}
           >
             <button onClick={() => props.setNotifications(props.notifications.filter( a => a.id !== notification.id))}>✕</button>
             {notification.message}
           </motion.div>
+          
         ))}
+        </AnimatePresence>
       </div>
     </>
   );
